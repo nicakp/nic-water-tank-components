cd84c5c4ddf0b66b046f9ad15f5632a80daabe85
nicakp <91095824+nicakp@users.noreply.github.com>
2025-05-11 04:15:25 +0630

    Update ultrasonic_sensor.h

--- Components/ultrasonic_sensor/ultrasonic_sensor.h
+++ Components/ultrasonic_sensor/ultrasonic_sensor.h
@@ -1,1 +1,21 @@
+#pragma once
 
+#include "esphome.h"
+
+class UltrasonicSensor : public Component, public UARTDevice {
+public:
+  UltrasonicSensor(UARTComponent *parent) : UARTDevice(parent) {}
+
+  void loop() override {
+    while (available()) {
+      uint8_t data[4];
+      if (read_array(data, 4) && data[0] == 0xFF && data[3] == (data[0] + data[1] + data[2]) % 256) {
+        int raw_distance_mm = (data[1] << 8) | data[2];
+        int water_level_mm = 300 - raw_distance_mm;
+        float current_level_0_1 = (float)std::max(0, std::min(water_level_mm, 250)) / 250.0;
+        id(water_level).publish_state(current_level_0_1);
+        id(raw_distance).publish_state(raw_distance_mm);
+      }
+    }
+  }
+};

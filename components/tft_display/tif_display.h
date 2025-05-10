e4d2b34ded04b9fb5e2a4422ca8f39714ba02080
nicakp <91095824+nicakp@users.noreply.github.com>
2025-05-11 03:32:34 +0630

    Update tif_display.h

--- components/tft_display/tif_display.h
+++ components/tft_display/tif_display.h
@@ -1,1 +1,14 @@
+#ifndef TFT_DISPLAY_H
+#define TFT_DISPLAY_H
 
+#include <TFT_eSPI.h>
+
+class TFTDisplay {
+ public:
+  TFT_eSPI tft = TFT_eSPI();
+
+  void setup();
+  void show_wifi_info(const char* ip_address, const char* mac_address);
+};
+
+#endif
